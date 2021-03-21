abc3ba8602c766f02dd2d20d9011c2cef8d2e63e
JeffChen <731590520@qq.com>
2021-03-21 12:00:16 +0800

    20210321

---
+++ BinaryTree/P4715_knockoutStage.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <stack>
+#include <cstring>
+#include <conio.h>
+#define fp(i, l, r) for(register int i=(l); i<=(r); i++)
+#define fd(i, l, r) for(register int i=(l); i>=(r); i--)
+typedef long long ll;
+
+using namespace std;
+
+int main(void) {
+    
+
+    _getch();
+    return 0;
+}
