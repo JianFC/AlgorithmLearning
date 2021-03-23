d4277abd0667eee5e13444bfb801bc9d55b31771
JeffChen <731590520@qq.com>
2021-03-23 23:49:25 +0800

    20210323

--- BinaryTree/P5076_commonBTree.cpp
+++ BinaryTree/P5076_commonBTree.cpp
@@ -103,8 +103,35 @@ int queryBack(int loc, int x, int ans) {
 }
 
 //查询数x的排名（排名定义为比当前数小的数的个数+1。若有多个相同的数，应输出最小的排名）
-int queryRankforVal(int loc, int x, int ans) {
-    
+int queryRankforVal(int loc, int x) {
+    if (loc == 0) return 0;
+    else {  //loc!=0
+        if (tree[loc].val == x) {
+            return tree[tree[loc].ls].siz;
+        }
+        else if (tree[loc].val < x) {
+            return queryRankforVal(tree[loc].rs, x)+tree[tree[loc].ls].siz + tree[loc].cnt;
+        }
+        else {
+            return queryRankforVal(tree[loc].ls, x);
+        }
+    }
+}
+
+//查询排名为x的数
+int queryValofRank(int loc, int x) {
+    if (loc == 0) return INF;
+    else {
+        if (tree[tree[loc].ls].siz >= x) {
+            return queryValofRank(tree[loc].ls, x);
+        }
+        else if (tree[tree[loc].ls].siz + tree[loc].cnt >= x) {
+            return tree[loc].val;
+        }
+        else {
+            return queryValofRank(tree[loc].rs, x-tree[tree[loc].ls].siz-tree[loc].cnt);
+        }
+    }
 }
 
 
@@ -112,7 +139,11 @@ int main(void) {
     cin >> q;
     fp(i, 1, q) {
         cin >> opt >> x;
-        if (opt == 5) {
+        if (opt == 1) cout << queryRankforVal(1, x)+1 << endl;
+        else if (opt == 2) cout << queryValofRank(1, x) << endl;
+        else if (opt == 3) cout << queryFront(1, x, -INF) << endl;
+        else if (opt == 4) cout << queryBack(1, x, INF) << endl;
+        else if (opt == 5) {
             if (cont==0) {  //当二叉树为空时，手动建立根节点
                cont++;
                tree[cont].val = x;
