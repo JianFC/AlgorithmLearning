1fd20734621cb3b0d6234e90442717feff05c9ae
JeffChen <731590520@qq.com>
2021-04-05 20:01:33 +0800

    20210405

--- CSP/201812-1.cpp
+++ CSP/201812-1.cpp
@@ -10,9 +10,23 @@
 typedef long long ll;
 
 using namespace std;
-
+int r, y, g;
+int n;
+int k, t;
+int res = 0;
 
 int main(void) {
+    cin >> r >> y >> g;
+    cin >> n;
+    fp(i, 1, n) {
+        cin >> k >> t;
+        switch(k){
+            case 0: res += t; break;
+            case 1: res += t; break;
+            case 2: res = res + t + r; break;
+        }
+    }
+    cout << res;
     
 
     _getch();
