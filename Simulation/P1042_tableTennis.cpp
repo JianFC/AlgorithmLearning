17f25b8dce597abb13ff746fb95f597e5c37b519
JeffChen <731590520@qq.com>
2021-04-10 15:17:24 +0800

    20210410

--- Simulation/P1042_tableTennis.cpp
+++ Simulation/P1042_tableTennis.cpp
@@ -15,7 +15,7 @@ using namespace std;
 const int maxn = 1e6+10;
 char arr[maxn];  //arr存储每个球的胜负信息,
 int cnt = 0;    //cnt是计数器
-int w, l;   //w表示一局中华华的得分，l表示对手的得分
+int w, l;   //w表示一局中华华的得分，l表示a对手的得分
  
 
 int main(void) {
@@ -24,24 +24,33 @@ int main(void) {
        arr[cnt++] = ch;
        cin >> ch;
     }
-    for (int i=0; i<cnt; i++) {
+    for (int i=0; 1; i++) {
        if (arr[i]=='W') w++;
        if (arr[i]=='L') l++;
-       if (((w>=11||l>=11)&&(abs(w-l)>=2)) || i==(cnt-1)) {
+       if ((w>=11||l>=11)&&(abs(w-l)>=2)) {
            cout << w << ":" << l << endl;
            w=0; l=0;
        }
+
+       if (i==cnt-1 || cnt == 0) {  //注意此处条件不能合并到上一个if条件中，否则在示例WWWWWWWWWWW（11个W）E中不会输出最后的0:0
+           cout << w << ":" << l << endl;
+           break;
+       }
     }
-    if (cnt==0) cout << 0 << ":" << 0 << endl;
+
     cout << endl;
-    if (cnt==0) cout << 0 << ":" << 0 << endl;
     w = l = 0;
-    for (int i=0; i<cnt; i++) {
+    for (int i=0; 1; i++) {
         if (arr[i]=='W') w++;
         if (arr[i]=='L') l++;
-        if (((w>=21||l>=21)&&(abs(w-l)>=2)) || i==(cnt-1)) {
+        if ((w>=21||l>=21)&&(abs(w-l)>=2)) {
             cout << w << ":" << l << endl;
             w=0; l=0;
+        }
+
+        if (i==cnt-1 || cnt == 0){
+            cout << w << ":" << l << endl;
+            break;
         }
     }
     
