b7ead26eb08468acb6ab4818a935f83e5e32ae46
JeffChen <731590520@qq.com>
2021-04-15 15:10:19 +0800

    20210415

---
+++ GraphTheory/P1395_conference.cpp
@@ -0,0 +1,21 @@
+//P1395 会议
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <conio.h>
+#define fp(i, l, r) for(register int i=(l); i<=(r); i++)
+#define fd(i, l, r) for(register int i=(l); i>=(r); i--)
+typedef long long ll;
+
+using namespace std;
+const int maxn = 5e4+10;
+
+int main(void) {
+    
+
+
+    _getch();
+    return 0;
+}
---
+++ GraphTheory/P3379_LCA.cpp
@@ -0,0 +1,21 @@
+// P3379 【模板】最近公共祖先（LCA）
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <conio.h>
+#define fp(i, l, r) for(register int i=(l); i<=(r); i++)
+#define fd(i, l, r) for(register int i=(l); i>=(r); i--)
+typedef long long ll;
+
+using namespace std;
+const int maxn = 5e5+10;
+
+int main(void) {
+    
+
+
+    _getch();
+    return 0;
+}
