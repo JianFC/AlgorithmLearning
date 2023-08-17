4701ba5d4fa5fccbb7610f7cf5b15acbee6e3522
JeffCheng <731590520@qq.com>
2023-08-17 22:03:38 +0800

    20230817

---
+++ AcWing/Level_2/Chapter1/ac0272_2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 3030;
+
+int n;
+int a[N], b[N];     //a和b数组分别代表两个序列
+int f[N][N];    //f[i][j]代表的集合：所有由a序列前i个字符，b序列前j个字符，且以b[j]
+                //结尾的公共上升子序列长度的集合; 集合属性为求长度最大值
+
+int main(void) {    //272. 最长公共上升子序列，优化做法，时间复杂度O(n^2)
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++) scanf("%d", &a[i]);
+    for (int i=1; i<=n; i++) scanf("%d", &b[i]);
+    
+    for (int i=1; i<=n; i++) {
+        
+        int maxv = 1;
+        for (int j=1; j<=n; j++) {
+            f[i][j] = f[i-1][j];
+            
+            if (b[j] < a[i]) maxv = max(maxv, f[i-1][j]+1); //求出f[i-1][k]+1的最大值
+            if (a[i] == b[j]) f[i][j] = max(f[i][j], maxv); //a[i]==b[j]时更新f[i][j];
+            
+            // if (a[i] == b[j]) {
+            //     f[i][j] = max(f[i][j], 1);
+            //     for (int k=1; k<j; k++)
+            //         if (b[k] < b[j]) //将b[j]换为a[i]后可以发现该循环可以和上层循环
+                                        //一起完成，故提出maxv变量，减少一层循环
+            //              f[i][j] = max(f[i][j], f[i-1][k]+1);
+            // }
+        }
+        
+    }
+    
+    int res = 0;
+    for (int i=1; i<=n; i++) res = max(res, f[n][i]);
+    
+    printf("%d", res);
+    
+    return 0;
+}
